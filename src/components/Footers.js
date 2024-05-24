9783888f4b4b7a18de825bee52d66357f2c0135d
portal222 <dvaportala@gmail.com>
2024-05-24 22:40:29 +0200

    footers

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -4,7 +4,7 @@ const Footers = () => {
 return (
     <div className="footer">
       <br></br>
-       The Art Institute Of Chicago® <a href="https://www.artic.edu/" >www.artic.edu</a>
+       The Art Institute Of Chicago® <a href="https://www.artic.edu/" target="_blanc">www.artic.edu</a>
          <br></br> 
          developer InTerVal
     </div>
