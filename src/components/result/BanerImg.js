6163269cf7d4cef7b78967f98c5b9cd4ecb9d87f
portal222 <dvaportala@gmail.com>
2024-05-27 21:46:11 +0200

    baner2

--- src/components/result/BanerImg.js
+++ src/components/result/BanerImg.js
@@ -3,7 +3,7 @@ import React from "react";
 const BanerImg = () => {
 
 
-    const image = ["../baner.png", "../baner2.png", "../baner3.png", "../baner4.png", "../baner5.png", "../baner6.png"]
+    const image = ["../../../img/baner.png", "../../../img/baner2.png", "../../../img/baner3.png", "../../../img/baner4.png", "../../../img/baner5.png", "../../../img/baner6.png"]
 
 const random = Math.floor(Math.random() * image.length);
     return (
